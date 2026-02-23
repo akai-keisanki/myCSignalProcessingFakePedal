0b9bd6f0d705ac227b4c8bcc0384dbabd8dea1a8
akai-keisanki <iago.ad@outlook.com>
2026-02-22 21:34:09 -0300

    fix(filters/pedal.c): fix effect list reading

--- main.c
+++ main.c
@@ -12,39 +12,39 @@ int32_t main()
 
   scanf("%s", filter_list);
 
-  fprintf(stderr, "Opening input.wav...");
+  fprintf(stderr, "Opening input.wav...\n");
   input_wav = fopen("input.wav", "rb");
-  fprintf(stderr, "Finished opening input.wav.");
+  fprintf(stderr, "Finished opening input.wav.\n");
   fprintf(stderr, "Opening output.wav...");
-  output_wav = fopen("ouput.wav", "wb");
-  fprintf(stderr, "Finished opening output.wav.");
+  output_wav = fopen("output.wav", "wb");
+  fprintf(stderr, "Finished opening output.wav.\n");
 
 
-  fprintf(stderr, "Reading input.wav...");
+  fprintf(stderr, "Reading input.wav...\n");
   fread(header, 1, 44, input_wav);
-  fprintf(stderr, "Finished reading input.wav.");
+  fprintf(stderr, "Finished reading input.wav.\n");
 
-  fprintf(stderr, "Reading output.wav...");
+  fprintf(stderr, "Reading output.wav...\n");
   fwrite(header, 1, 44, output_wav);
-  fprintf(stderr, "Finished reading output.wav.");
+  fprintf(stderr, "Finished reading output.wav.\n");
   
 
-  fprintf(stderr, "Beginning filter application from input.wav to output.wav...");
+  fprintf(stderr, "Beginning filter application from input.wav to output.wav...\n");
   apply_filters(output_wav, input_wav, filter_list);
-  fprintf(stderr, "Finished filter application from input.wav to output.wav.");
+  fprintf(stderr, "Finished filter application from input.wav to output.wav.\n");
 
 
-  fprintf(stderr, "Closing input.wav...");
+  fprintf(stderr, "Closing input.wav...\n");
   fclose(input_wav);
-  fprintf(stderr, "Finished closing input.wav.");
+  fprintf(stderr, "Finished closing input.wav.\n");
 
-  fprintf(stderr, "Closing output.wav...");
+  fprintf(stderr, "Closing output.wav...\n");
   fclose(output_wav);
-  fprintf(stderr, "Finished closing output.wav.");
+  fprintf(stderr, "Finished closing output.wav.\n");
 
 
-  fprintf(stderr, "Finished! Output file complete.");
+  fprintf(stderr, "Finished! Output file complete.\n");
 
-  fprintf(stderr, "Exiting...");
+  fprintf(stderr, "Exiting...\n");
   return 0;
 }

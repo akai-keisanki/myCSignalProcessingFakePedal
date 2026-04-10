3634795933c188e8807342fe3f917ec9434b8718
akai-keisanki <iago.ad@outlook.com>
2026-04-10 16:27:05 -0300

    fix(filters/delay): make record size proportional to the required delay.

--- filters/delay.c
+++ filters/delay.c
@@ -2,9 +2,12 @@
 
 #include "util_time_recorder.h"
 
+#include <stddef.h>
+#include <math.h>
+
 float delay(struct filter *self, float x)
 {
-  unsigned dly = get_param(self, 0);
+  size_t dly = get_param(self, 0);
   float fdb = get_param(self, 1);
   float mix = get_param(self, 2);
   struct record_data *record = get_record_data(self);
@@ -17,5 +20,5 @@ float delay(struct filter *self, float x)
 
 struct filter *init_filter_delay(float dly, float fdb, float mix)
 {
-  return init_filter("delay", delay, (params_t){dly, fdb, mix}, (params_t){}, 100000);
+  return init_filter("delay", delay, (params_t){dly, fdb, mix}, (params_t){}, (size_t)ceil(dly) + 0x10);
 }
